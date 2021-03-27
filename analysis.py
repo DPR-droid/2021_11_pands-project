b0a125b745aa6edaa93f8666ee4f91ad02c23b4f
Ryan <G00398318@gmit.ie>
2021-03-27 22:10:59 +0000

    Python read file add header names

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Read data from archive.ics.uci.edu
-data = pd.read_csv("iris.data",header=None)
+data = pd.read_csv("iris.data",header=None, names=["sepal.length","sepal.width","petal.length","petal.width","variety"])
 
 # Print data
 print(data)
